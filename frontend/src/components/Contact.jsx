ee20315d44a3f05204812da9f54fb1097e3879cd
Tech_fbi <oluwafemijohnson347@gmail.com>
2026-01-31 10:42:31 +0100

    updated contact

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -3,7 +3,7 @@ const Contact = () => {
     <div className="contact-page">
       <h2>Contact Us</h2>
       <p>Feel free to reach out to Johnson for any inquiries.</p>
-      <strong>Email: &nbsp;</strong> <span>johnson@example.com</span>
+      <strong>Email: &nbsp;</strong> <span>oluwafemijohnson347@gmail.com</span>
     </div>
   );
 };
